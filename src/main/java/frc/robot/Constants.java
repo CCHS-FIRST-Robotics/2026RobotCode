d2b99959b940de6f8dc20b7a10cf60644f4c2cd8
AlextheChild <alexjun2008@gmail.com>
2026-03-09 00:15:37 -0400

    push for finn to practice to

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,7 @@ public final class Constants {
 
     public static final int CONTROLLER_PORT = 0;
 
-    public static final boolean TRENCH_ALIGN = false;
+    public static final boolean TRENCH_ALIGN = true;
 
     public static final Angle PIVOT_START_ANGLE = FuelConstants.PIVOT_MAX_DOWN_ANGLE; // ! remember to change this for comp
     public static final Angle HOOD_START_ANGLE = Rotations.of(0);
